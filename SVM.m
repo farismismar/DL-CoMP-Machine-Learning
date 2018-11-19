2b14ea332e3931be39896fcaba46505db087340d
farismismar <faris_mismar@hotmail.com>
2018-11-18 22:26:16 -0600

    Add files via upload

--- SVM.m
+++ SVM.m
@@ -2,19 +2,17 @@
 function [error, SVMModel] = SVM(x1,x2,x3)
 global epsilon;
 
-rng(0);
-
 %newX has the format [CQI, RSRP]
 X = [x1,x2];
 Y = (x3 <= 0.1);  % Did BLER target fulfillment succeed?
 
-% split = 0.6;
-% 
-% % Split 60-40
-% X_training = X(1:round(length(X)*split),:);
-% Y_training = Y(1:round(length(Y)*split),:);
-% X_test = X(round(length(X)*split)+1:end,:);
-% Y_test = Y(round(length(Y)*split)+1:end,:);
+split = 0.7;
+ 
+% Split 
+X_training = X(1:round(length(X)*split),:);
+Y_training = Y(1:round(length(Y)*split),:);
+X_test = X(round(length(X)*split)+1:end,:);
+Y_test = Y(round(length(Y)*split)+1:end,:);
 
 X_training = X;
 Y_training = Y;
@@ -37,9 +35,9 @@ SVMModel = fitcsvm(X_training, Y_training, ...
     'HyperparameterOptimizationOptions',struct('MaxObjectiveEvaluations', 5, ...
         'ShowPlots', false)); 
 
-% Perform K-Fold Cross Validation
-CVSVMModel = crossval(SVMModel);
-error = 1 - mean(kfoldPredict(CVSVMModel) == Y_training);
+% Test error and kFoldPrediction
+CVSVMModel = predict(SVMModel, X_test);
+error = 1 - mean(kfoldPredict(CVSVMModel) == Y_test);
 
 fprintf('CoMP Cluster: Classification error is %0.1f%%.\n', error * 100);
 
